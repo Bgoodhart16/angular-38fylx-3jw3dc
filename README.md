5dc5ca8710371e2baaf1d2b992f3083820ee7f92
Bgoodhart16 <bengoodhart16@gmail.com>
2022-06-10 12:05:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-38fylx-3jw3dc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-38fylx-3jw3dc)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products'; //This is importing the products array.
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;    //This is defining a property that contains imported data from the prod array
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
